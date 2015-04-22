cef40f3eecb91c510dccbb2167ad3e11de2d7641
Coen Jacobs <coenjacobs@gmail.com>
2015-04-22 16:00:53 +0200

    Plugin header details

--- rickroll-admin-login.php
+++ rickroll-admin-login.php
@@ -2,6 +2,10 @@
 
 /**
  * Plugin Name: Rickroll Admin Login
+ * Description: Redirects the user trying to log in with 'admin' as username, to the world famous Rickroll video on YouTube.
+ * Author: Coen Jacobs
+ * Author URI: http://coenjacobs.me
+ * Version: 1.0
  */
 
 add_action( 'authenticate', 'rickroll_check_admin_login', 1, 2);
